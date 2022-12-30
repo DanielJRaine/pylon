b69e70c08b497bc410c3974eb04d4099ab90ffcc
Daniel Raine <danieljraine@gmail.com>
2022-12-30 10:00:32 -0500

    add cloudflare SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  output: "server",
+  adapter: cloudflare()
 });
